8a37d27d806e3dd235275aba3f11dd97251212d4
Tőzsér Tamás <tamas.tozser@doctusoft.com>
2014-05-24 14:33:47 +0200

    index the descriptions

--- src/main/java/com/doctusoft/cityguide/service/PlaceService.java
+++ src/main/java/com/doctusoft/cityguide/service/PlaceService.java
@@ -1,12 +1,15 @@
 package com.doctusoft.cityguide.service;
 
+import com.doctusoft.cityguide.entity.Card;
 import com.doctusoft.cityguide.entity.Place;
 import com.google.appengine.api.search.Document;
+import com.google.appengine.api.search.Document.Builder;
 import com.google.appengine.api.search.Field;
 import com.google.appengine.api.search.GeoPoint;
 import com.google.appengine.api.search.Index;
 import com.google.appengine.api.search.IndexSpec;
 import com.google.appengine.api.search.SearchServiceFactory;
+import com.googlecode.objectify.Ref;
 
 public class PlaceService extends EntityDao<Place> {
 	
@@ -16,15 +19,22 @@ public class PlaceService extends EntityDao<Place> {
 	}
 	
 	@Override
-	public Place save(Place card) {
-		card = super.save(card);
-		Document indexEntry = Document.newBuilder().setId(card.getId())
-				.addField(Field.newBuilder().setName("location").setGeoPoint(new GeoPoint(card.getLocation().getLatitude(), card.getLocation().getLongitude())).build())
-				.addField(Field.newBuilder().setName("name").setText(card.getName()).build())
-				.build();
+	public Place save(Place place) {
+		place = super.save(place);
+		Builder builder = Document.newBuilder().setId(place.getId())
+				.addField(Field.newBuilder().setName("location").setGeoPoint(new GeoPoint(place.getLocation().getLatitude(), place.getLocation().getLongitude())).build())
+				.addField(Field.newBuilder().setName("name").setText(place.getName()).build());
+		for (Ref<Card> card : place.getCards()) {
+			for (String description : card.get().getProperties().values()) {
+				if (description != null) {
+					builder.addField(Field.newBuilder().setName("description").setText(description).build());
+				}
+			}
+		}
+		Document indexEntry = builder.build();
 		IndexSpec indexSpec = IndexSpec.newBuilder().setName("places").build();
 		Index index = SearchServiceFactory.getSearchService().getIndex(indexSpec);
 		index.put(indexEntry);
-		return card;
+		return place;
 	}
 }
